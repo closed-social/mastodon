8fe8ae031079f69e0875960aa977c2f208cb6a73
Tdxdxoz <tdxdxoz@gmail.com>
2026-09-07 02:15:52 +0800

    Mask email for admin and remove changing email

--- app/helpers/admin/accounts_helper.rb
+++ app/helpers/admin/accounts_helper.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module Admin::AccountsHelper
+  def admin_account_email(account)
+    email = account.user_email
+    return email unless account.user_confirmed? && email.present?
+
+    local_part, _, domain = email.rpartition('@')
+    "#{local_part[0]}***#{local_part[-1]}@#{domain}"
+  end
+
   def admin_accounts_moderation_options
     [
       [t('admin.accounts.moderation.active'), 'active'],
